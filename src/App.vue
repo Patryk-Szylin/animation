9b21ea6c1379b5cc33dce39c9a72ecbab210bfa8
Patryk <patryk.szylin.dev@gmail.com>
2022-04-04 20:09:54 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,140 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="containe/r">
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+
+    <canvas id="myCanvas" ></canvas>
+
+    <div>{{i}}</div>
+  <div id="box" style="left: 200;"></div>
+  <!-- <div id="box2" style="left: 200;"></div> -->
+</div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {Point , normalize, cubicBezier, quadraticBezier, qBezier, linear } from "./lib";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data() {
+      return {
+          i: 0
+      }
+  },
+  mounted() {
+    console.log("hello")
+
+    var p1 = new Point(0, 0);
+    var p2 = new Point(0.5, -1);
+    var p3 = new Point(1,-1);
+    // var p4 = new Point(1,1);
+    var t = 0;
+
+    const animateable = document.getElementById('box');
+    const animateable2 = document.getElementById('box2');
+
+
+    document.onscroll = (e) => {
+        // var someElement = normalize(window.scrollY, 100, 500);
+        // var finalPoint = {};
+        // // var coords = quadraticBezier(p1, p2, p3, someElement);
+        // qBezier(p1, p2, p3, someElement, finalPoint);
+
+        // if(someElement > 0 && someElement <= 1) {
+        //     animateable.style.transform = 'translate(' + (finalPoint.x * 50) + 'px,' + (finalPoint.y * 50) + 'px)';
+        // }
+    }
+
+    this.i = 0;
+
+
+    // animateable.style.transform = 'translate'
+
+    var destX = window.innerWidth;
+    var destY = 200;
+
+    console.log('destX', normalize(600, 0, window.innerWidth));
+
+    // Unique per object
+    var p1 = new Point(0.5, 0);
+    var p2 = new Point(0, -1);
+    var p3 = new Point(-0.5,-1);
+
+
+
+
+    // var c = document.getElementById("myCanvas");
+    // var ctx = c.getContext("2d");
+    // ctx.beginPath();
+    // ctx.arc(25, 25, 10, 0, 2 * Math.PI);
+    // ctx.stroke();
+
+    var offset = normalize(this.i, -200, 600);
+
+    console.log('offset', offset)
+
+    setInterval(() => {
+      
+      if(this.i <= 500) {
+
+        var finalPoint = {};
+        var finalPoint2 = {};
+        var t = normalize(this.i, 0, 200);
+        var t2 = normalize(this.i, 0, 200);
+        qBezier(p1, p2, p3, t, finalPoint);
+        qBezier(p1, p2, p3, t2, finalPoint2);
+        // var l = linear(p1,p2, t);
+        console.log('asd')
+
+        const offset = normalize(this.i, -200, 600);
+
+        console.log('offset', offset)
+
+        const abs = Math.abs(-200 - 600 );
+
+        console.log('abs', abs)
+
+        if(t > 0 && t <= 1) {
+
+            animateable.style.transform = 'translate(' +  (finalPoint.x * 400) + 'px,' + (finalPoint.y * 200) + 'px) ';
+            // animateable2.style.transform = 'translate(' +  (finalPoint2.x * -400) + 'px,' + (finalPoint2.y * 200) + 'px) ';;
+        }
+
+
+        this.i++;
+      }
+
+
+    }, 10);
+
   }
 }
 </script>
 
 <style>
+.container {
+    /* display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center; */
+    height: 4000px;
+    background-color: lightgray;
+}
+#box, #box2 {
+    width: 100px;
+    height: 100px;
+    background-color: red;
+    position: absolute;
+    top: 500px;
+    left: 500px;
+}
+
+#box2 {
+    background-color: green;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/lib/index.js
@@ -0,0 +1,125 @@
+
+
+
+/**
+ * Bezier stuff
+ */
+
+
+// var p1 = new Point(0, 0);
+// var p2 = new Point(0.5, 0.5);
+// var p3 = new Point(1, 0);
+// var p4 = new Point(1,1);
+
+
+export class Point {
+    constructor(_x, _y) {
+        this.x = _x;
+        this.y = _y;
+    }
+}
+
+export const normalize = (value, min, max) => (value - min) / (max - min);
+
+
+export const cubicBezier = (p1, p2, p3, p4, t) => {
+    var coordX = Math.pow(1 - t, 3) * p1.x + 3 * Math.pow(1 - t, 2) * t * p2.x + 3 * (1 - t) * Math.pow(t, 2) * p3.x + Math.pow(t, 3) * p4.x;
+
+    var coordY = Math.pow(1 - t, 3) * p1.y + 3 * Math.pow(1 - t, 2) * t * p2.y + 3 * (1 - t) * Math.pow(t, 2) * p3.y + Math.pow(t, 3) * p4.y;
+
+    return {
+        x: coordX,
+        y: coordY
+    };
+}
+
+export const quadraticBezier = (p1, p2, p3, t) => {
+    var coordX = Math.pow(1 - t, 2) * p1.x + 2 * (1 - t) * t * p2.x + Math.pow(t, 2) * p3.x;
+
+    // y = (1−t)2y1 + 2(1−t)ty2 + t2y3
+    var coordY = Math.pow(1 - t, 2) * p1.y + 2 * (1 - t) * t * p2.y + Math.pow(t, 2) * p3.y;
+
+    return {
+        x: coordX,
+        y: coordY
+    }
+}
+
+export const qBezier = (p0, p1, p2, t, pFinal = {}) => {
+    var pFinal = pFinal || {};
+
+    pFinal.x    = Math.pow(1 - t, 2) * p0.x + 
+                    (1 - t) * 2 * t * p1.x +
+                    t * t * p2.x;
+
+    pFinal.y    =  Math.pow(1 - t, 2) * p0.y + 
+                    (1 - t) * 2 * t * p1.y +
+                    t * t * p2.y;
+
+    return pFinal;
+}
+
+export const linear = (p0, p1, t) => {
+    const s = (1 - t) * p0.x + t * p1.x;
+
+    return s;
+}
+
+// const animateable2 = document.getElementById('box2');
+
+// export default (() => {
+
+
+    
+    
+
+
+
+   
+
+
+//     document.onscroll = (e) => {
+
+
+//         var someElement = normalize(window.scrollY, 100, 300);
+//         // var someElement2 = normalize(window.scrollY, 310, 600);
+//         var speed = 500;
+
+
+
+//         if(someElement > 0 && someElement <= 1) {
+//             // Animate
+//             // animateable.style.transform = 'translateX(' + someElement * 0.2 + 'px);';
+//             // animateable.style.left = (someElement * 100) + 'px';
+
+//             // // (1−t)2x1 + 2(1−t)tx2 + t2x3
+//             // var coordX = Math.pow(1 - someElement, 2) * p1.x + 2 * (1 - someElement) * someElement * p2.x + Math.pow(someElement, 2) * p3.x;
+
+//             // // y = (1−t)2y1 + 2(1−t)ty2 + t2y3
+//             // var coordY = Math.pow(1 - someElement, 2) * p1.y + 2 * (1 - someElement) * someElement * p2.y + Math.pow(someElement, 2) * p3.y;
+
+
+//             //
+//             // var coordX = Math.pow(1 - someElement, 3) * p1.x + 3 * Math.pow(1 - someElement, 2) * someElement * p2.x + 3 * (1 - someElement) * Math.pow(someElement, 2) * p3.x + Math.pow(someElement, 3) * p4.x;
+
+//             // var coordY = Math.pow(1 - someElement, 3) * p1.y + 3 * Math.pow(1 - someElement, 2) * someElement * p2.y + 3 * (1 - someElement) * Math.pow(someElement, 2) * p3.y + Math.pow(someElement, 3) * p4.y;
+
+//             var coords = cubicBezier(p1, p2, p3, p4, someElement);
+//             var coords = quadraticBezier(p1, p2, p3, someElement);
+
+
+//             // animateable.style.left = coordX + 'px';
+
+//             animateable.style.transform = 'translate(' + (coords.x * 200) + 'px,' + (coords.y * 200) + 'px)';
+            
+
+
+
+//         }
+
+//     // if(someElement2 > 0 && someElement2 <= 1) {
+//     //     animateable2.style.left = (someElement2 * speed) + 'px';
+//     // }
+// }
+
+// })();
